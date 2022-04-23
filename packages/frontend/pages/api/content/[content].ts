e93a30a4bcf7e8b54e32a22b8ad706e9d3291b9d
Dennis Zoma <wottpal@users.noreply.github.com>
2022-04-23 11:19:21 +0200

    build mongodb routes for create + read posts

---
+++ packages/frontend/pages/api/content/[content].ts
@@ -0,0 +1,83 @@
+import { Db, MongoClient } from 'mongodb'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { connectToDatabase } from '../../../lib/mongodb'
+
+export interface Post {
+  id: string
+  date: string
+  owner: string
+  title: string
+  content: string
+}
+
+export type MongoDBConnection = { db: Db; client: MongoClient }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { content: slug } = req.query
+
+  switch (slug) {
+    case 'create':
+      return await handleCreateContent(req, res)
+    case 'get':
+      return await handleGetContent(req, res)
+    default:
+      return res.status(404).end()
+  }
+}
+
+export const handleCreateContent = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const post = req.body || {}
+  if (!post?.owner || !post?.title || !post?.content)
+    return res.status(400).end()
+
+  const { owner, title, content } = post
+  const newPost = {
+    date: new Date().toUTCString(),
+    owner,
+    title,
+    content,
+  }
+
+  const { db } = (await connectToDatabase()) as MongoDBConnection
+
+  db.collection('posts').insertOne(newPost, (error, response) => {
+    const id = response?.insertedId
+    if (error || !id) {
+      console.error('Error while saving post', newPost, error)
+      return res.status(500).end()
+    }
+    console.log('New post inserted', id, newPost)
+    return res.status(200).json({
+      id,
+      ...newPost,
+    })
+  })
+}
+
+export const handleGetContent = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const { owner } = req.body || {}
+  if (!owner) return res.status(400).end()
+
+  const { db } = (await connectToDatabase()) as MongoDBConnection
+  const posts = await db
+    .collection('posts')
+    .find({ owner })
+    .sort({ date: -1 })
+    // .limit(20)
+    .toArray()
+
+  console.log('Fetched posts:', posts)
+
+  return res.status(200).json({
+    posts,
+  })
+}
--- packages/frontend/pages/users/[walletId].tsx
+++ packages/frontend/pages/users/[walletId].tsx
@@ -1,10 +1,23 @@
+import { useEthers } from '@usedapp/core'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function UsersPage() {
   const router = useRouter()
   const { walletId } = router.query
+  const { account } = useEthers()
+  const [isMyPage, setIsMyPage] = useState(false)
 
-  return ( 
+  useEffect(() => {
+    if (!walletId || !account) {
+      setIsMyPage(false)
+      return
+    }
+    setIsMyPage(walletId === account)
+  }, [account])
+
+  return <>
     <>{walletId}</>
-  )
+    <>{isMyPage ? 'IS MY PAGE' : 'IS NOT MY PAGE'}</>
+  </>
 }
