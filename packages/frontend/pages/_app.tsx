26378cb3e4068e9456bbbdebd0708e790e4115f7
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-24 00:36:58 +0200

    Remove apollo from _app

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,11 +1,13 @@
-import { ApolloProvider } from '@apollo/client'
+import * as React from 'react'
+import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ChainId } from '@usedapp/core'
 import { providers } from 'ethers'
-import type { AppProps } from 'next/app'
-import React from 'react'
-import { defaultChains, InjectedConnector, Provider as WagmiProvider } from 'wagmi'
-import { useApollo } from '../lib/apolloClient'
+import {
+  defaultChains,
+  InjectedConnector,
+  Provider as WagmiProvider,
+} from 'wagmi'
 import { env } from '../lib/environment'
 
 // scaffold-eth's INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
@@ -24,7 +26,9 @@ export const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
 //   },
 // }
 
-const chains = defaultChains.filter((chain) => (env.supportedChains || []).includes(chain.id))
+const chains = defaultChains.filter((chain) =>
+  (env.supportedChains || []).includes(chain.id)
+)
 const connectors = ({ chainId }) => {
   return [
     new InjectedConnector({
@@ -37,26 +41,23 @@ const connectors = ({ chainId }) => {
 }
 
 const provider = ({ chainId }) => {
-  if (chainId === ChainId.Localhost || chainId === 31337) return new providers.JsonRpcProvider(env.rpc.hardhat)
+  if (chainId === ChainId.Localhost || chainId === 31337)
+    return new providers.JsonRpcProvider(env.rpc.hardhat)
   // if (chainId === ChainId.Mumbai) return new providers.JsonRpcProvider(env.rpc.hardhat)
   // if (chainId === ChainId.Mainnet) return new providers.JsonRpcProvider(env.rpc.hardhat)
   // if (chainId === ChainId.Mumbai) return new providers.JsonRpcProvider(env.rpc.polygonMumbai)
-  if (chainId === ChainId.Mumbai) return new providers.AlchemyProvider(chainId, env.alchemyApiKeys.mumbai)
+  if (chainId === ChainId.Mumbai)
+    return new providers.AlchemyProvider(chainId, env.alchemyApiKeys.mumbai)
   return new providers.InfuraProvider(chainId, env.infuraApiKey)
 }
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  const apolloClient = useApollo(pageProps)
   return (
-    <ApolloProvider client={apolloClient}>
-      {/* <DAppProvider config={dappConfig}> */}
-      <WagmiProvider autoConnect connectors={connectors} provider={provider}>
-        <ChakraProvider>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </WagmiProvider>
-      {/* </DAppProvider> */}
-    </ApolloProvider>
+    <WagmiProvider autoConnect connectors={connectors} provider={provider}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </WagmiProvider>
   )
 }
 
