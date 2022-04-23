3238a9544c6ba1d2aa9c990233bf31130a541a97
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-23 15:26:02 +0200

    WIP homepage and layout

--- packages/frontend/components/layout/Layout.tsx
+++ packages/frontend/components/layout/Layout.tsx
@@ -5,6 +5,7 @@ import {
   AlertTitle,
   Box,
   Container,
+  Flex,
 } from '@chakra-ui/react'
 import { useNotifications } from '@usedapp/core'
 import React from 'react'
@@ -38,38 +39,36 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
       <header>
         <Navbar />
       </header>
-      <main>
-        <Container maxWidth="container.xl">
-          {children}
-          {notifications.map((notification) => {
-            if (notification.type === 'walletConnected') {
-              return null
-            }
-            return (
-              <Alert
-                key={notification.id}
-                status="success"
-                position="fixed"
-                bottom="8"
-                right="8"
-                width="400px"
-              >
-                <AlertIcon />
-                <Box>
-                  <AlertTitle>
-                    {TRANSACTION_TITLES[notification.type]}
-                  </AlertTitle>
-                  <AlertDescription overflow="hidden">
-                    Transaction Hash:{' '}
-                    {/* {truncateHash(notification.transaction.hash, 61)} */}
-                  </AlertDescription>
-                </Box>
-              </Alert>
-            )
-          })}
-        </Container>
-      </main>
-      <Box as="footer">Built with ❤️ on top Aave, Polygon and others.</Box>
+      <Box as="main">
+        {children}
+        {notifications.map((notification) => {
+          if (notification.type === 'walletConnected') {
+            return null
+          }
+          return (
+            <Alert
+              key={notification.id}
+              status="success"
+              position="fixed"
+              bottom="8"
+              right="8"
+              width="400px"
+            >
+              <AlertIcon />
+              <Box>
+                <AlertTitle>{TRANSACTION_TITLES[notification.type]}</AlertTitle>
+                <AlertDescription overflow="hidden">
+                  Transaction Hash:{' '}
+                  {/* {truncateHash(notification.transaction.hash, 61)} */}
+                </AlertDescription>
+              </Box>
+            </Alert>
+          )
+        })}
+      </Box>
+      <Flex as="footer" borderTop="1px" p={2} justify="center">
+        Built with ❤️ on top Aave, Polygon and others.
+      </Flex>
     </>
   )
 }
--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,30 +1,107 @@
-import { Text } from '@chakra-ui/react'
+import {
+  Box,
+  Text,
+  Heading,
+  Button,
+  Flex,
+  Image,
+  Grid,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react'
 import Layout from '@components/layout/Layout'
 import { Creator } from '@entities/Creator.entity'
 import { env } from '@lib/environment'
 import { GetServerSideProps } from 'next'
 import Link from 'next/link'
+import { BsSearch } from 'react-icons/bs'
 
 export interface IndexPageProps {
   creators: Creator[]
 }
-export default function IndexPage({creators}: IndexPageProps) {
-  return <>
-    <Layout>
-      {creators.map((creator) => <Text key={creator._id} decoration={'underline'}>
-        <Link href={`/users/${creator.address}`}>{creator.address}</Link>
-      </Text>)}
-      {JSON.stringify(creators)}
 
+export default function IndexPage({ creators }: IndexPageProps) {
+  return (
+    <Layout>
+      <Box
+        bgImage="images/banner-background.png"
+        h="250px"
+        bgPos="center"
+        bgSize="cover"
+      >
+        <Heading>Yieldgate</Heading>
+        <Text>
+          Get your fans support you by letting them stake coins for your benefit
+        </Text>
+        <Button>Create on Yieldgate</Button>
+      </Box>
+      <Flex direction="column">
+        <Heading>What's Yieldgate?</Heading>
+        <Text>
+          Vivamus in scelerisque nunc. Ut quis turpis venenatis, gravida tortor
+          ut, suscipit mi. Proin cursus porttitor justo eget maximus. Morbi
+          auctor lacus id placerat luctus. Phasellus sagittis ultricies elit,
+          nec egestas odio interdum vel. Aenean porta porta ligula, vel iaculis
+          erat condimentum sit amet. Pellentesque posuere sem sit amet feugiat
+          mollis.
+        </Text>
+      </Flex>
+      <Flex direction="column">
+        <Heading>How does it work?</Heading>
+        <Grid gridTemplateColumns="repeat(3, 1fr)" gap={10}>
+          <Flex direction="column" border="1px">
+            <Image fallbackSrc="https://via.placeholder.com/150" />
+            <Text p={5}>
+              Vivamus in scelerisque nunc. Ut quis turpis venenatis, gravida
+              tortor ut, suscipit mi.
+            </Text>
+          </Flex>
+          <Flex direction="column" border="1px">
+            <Image fallbackSrc="https://via.placeholder.com/150" />
+            <Text p={5}>
+              Aenean porta porta ligula, vel iaculis erat condimentum sit amet.
+              Pellentesque posuere sem sit amet feugiat mollis.
+            </Text>
+          </Flex>
+          <Flex direction="column" border="1px">
+            <Image fallbackSrc="https://via.placeholder.com/150" />
+            <Text p={5}>
+              Phasellus sagittis ultricies elit, nec egestas odio interdum vel.
+            </Text>
+          </Flex>
+        </Grid>
+      </Flex>
+      <Flex direction="column">
+        <Heading>Sponsor a creator</Heading>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<BsSearch size={20} />}
+          />
+          <Input type="tel" placeholder="Find creator" />
+        </InputGroup>
+      </Flex>
+      <Box as="details" my={5}>
+        <summary>Creators</summary>
+        {creators.map((creator) => (
+          <Text key={creator._id} decoration={'underline'}>
+            <Link href={`/users/${creator.address}`}>{creator.address}</Link>
+          </Text>
+        ))}
+        <Box as="pre" overflow="scroll" bg="gray.200">
+          <code>{JSON.stringify(creators, null, 2)}</code>
+        </Box>
+      </Box>
     </Layout>
-  </>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const res = await fetch(`${env.url}/api/creators/getAllCreators`,{
+  const res = await fetch(`${env.url}/api/creators/getAllCreators`, {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
   })
 
@@ -37,4 +114,3 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     // revalidate: 60,
   }
 }
-
