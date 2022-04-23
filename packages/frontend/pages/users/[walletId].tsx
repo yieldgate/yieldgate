afc1275731935533362219eba47d503b7da66207
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-23 19:39:23 +0200

    Test if removing useEthers removes client error
    I think wagmi and useEthers might be conflicting if they're both
    injecting something into the global browser scope? Not sure, let's see.

--- packages/frontend/pages/users/[walletId].tsx
+++ packages/frontend/pages/users/[walletId].tsx
@@ -2,34 +2,36 @@ import { BlockiesAvatar } from '@components/BlockiesAvatar'
 import Feed from '@components/Feed'
 import Layout from '@components/layout/Layout'
 import { Creator } from '@entities/Creator.entity'
-import { useEthers } from '@usedapp/core'
 import { useRouter } from 'next/router'
 import * as React from 'react'
 import { useEffect, useState } from 'react'
 import { useAsyncEffect } from 'use-async-effect'
 
-
 export default function UsersPage() {
   const router = useRouter()
   let { walletId } = router.query
-  if (typeof walletId !== 'string' || !/^0x[a-fA-F0-9]{40}$/.test(walletId as string)) {
+  if (
+    typeof walletId !== 'string' ||
+    !/^0x[a-fA-F0-9]{40}$/.test(walletId as string)
+  ) {
     return <>Not a valid address</>
   }
   walletId = walletId.toLowerCase()
-  
-  const { account, chainId } = useEthers()
+
+  // const { account } = useEthers()
+  const account = undefined
   const [isMyPage, setIsMyPage] = useState(false)
   const [creator, setCreator] = useState<Creator | null>(null)
 
   const fetchCreator = async (): Promise<Creator> => {
-    const res = await fetch('/api/creators/getCreator',{
+    const res = await fetch('/api/creators/getCreator', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         address: walletId,
-      })
+      }),
     })
 
     const { creator }: { creator: Creator } = await res.json()
@@ -52,10 +54,12 @@ export default function UsersPage() {
     setCreator(creator)
   }, [walletId])
 
-  return <>
-    <Layout>
-      <BlockiesAvatar address={walletId} ml="4" width={250} height={250} />
-      <Feed feed={creator?.posts || []} isLocked={false} />
-    </Layout>
-  </>
+  return (
+    <>
+      <Layout>
+        <BlockiesAvatar address={walletId} ml="4" width={250} height={250} />
+        <Feed feed={creator?.posts || []} isLocked={false} />
+      </Layout>
+    </>
+  )
 }
