8c80554be05c8d7e9a1e0c481aa0800abe219364
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-23 21:02:17 +0200

    Improve project card

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -11,6 +11,8 @@ import {
   InputLeftElement,
   Container,
   Link,
+  Spacer,
+  HStack,
 } from '@chakra-ui/react'
 import Layout from '@components/layout/Layout'
 import { Creator } from '@entities/Creator.entity'
@@ -18,6 +20,7 @@ import { env } from '@lib/environment'
 import { GetServerSideProps } from 'next'
 import NextLink from 'next/link'
 import { BsSearch } from 'react-icons/bs'
+import { BlockiesAvatar } from '@components/BlockiesAvatar'
 
 export interface IndexPageProps {
   creators: Creator[]
@@ -27,11 +30,34 @@ function CreatorCard(props: Creator): JSX.Element {
   return (
     <NextLink href={`/users/${props.address}`} passHref>
       <Link>
-        <Flex direction="column" border="1px" p={8} borderRadius="md">
-          <Text key={props._id} decoration={'underline'}></Text>
-          <Box as="pre" overflow="scroll" bg="gray.200">
-            <code>{JSON.stringify(props, null, 2)}</code>
-          </Box>
+        <Flex direction="column" bg="gray.100" p={8} borderRadius="md">
+          <Flex align="center">
+            <BlockiesAvatar
+              address={props.address}
+              borderRadius="full"
+              width="100px"
+              height="100px"
+            />
+            <Flex direction="column" align="left" mx={8}>
+              <Heading>{props.displayName}</Heading>
+              <Text>{props.description}</Text>
+            </Flex>
+            <Spacer />
+            <HStack spacing="24px" mx={8}>
+              <Flex direction="column" align="center">
+                <Heading>{props.supportersCount}</Heading>
+                <Text>Supporters</Text>
+              </Flex>
+              {/* <Flex direction="column" align="center">
+                <Heading>{props.supportersCount}</Heading>
+                <Text>ETH staked</Text>
+              </Flex> */}
+              <Flex direction="column" align="center">
+                <Heading>{props.postsCount}</Heading>
+                <Text>Posts</Text>
+              </Flex>
+            </HStack>
+          </Flex>
         </Flex>
       </Link>
     </NextLink>
@@ -49,9 +75,7 @@ export default function IndexPage({ creators }: IndexPageProps) {
         <Container maxW="5xl" py={24}>
           <Flex direction="column" align="start" gap={3}>
             <Image src="/images/logo-yieldgate-long.svg" h="70px" mb={12} />
-            <Text fontSize="3xl">
-              Let your fans stake coins on your behalf
-            </Text>
+            <Text fontSize="3xl">Let your fans stake coins on your behalf</Text>
             <Button size="lg">Create on Yieldgate</Button>
           </Flex>
         </Container>
@@ -62,7 +86,10 @@ export default function IndexPage({ creators }: IndexPageProps) {
             What's Yieldgate?
           </Heading>
           <Text maxW="50em" alignSelf="center" fontSize="xl">
-          Yieldgate is a monetisation tool for anyone to start receiving donations, or to support their favourite public goods projects, creators, and security researchers, with yield. No NFTs or tokens, just good old MATIC! Built with ❤️ on top of Aave, Polygon.
+            Yieldgate is a monetisation tool for anyone to start receiving
+            donations, or to support their favourite public goods projects,
+            creators, and security researchers, with yield. No NFTs or tokens,
+            just good old MATIC! Built with ❤️ on top of Aave, Polygon.
           </Text>
         </Flex>
         <Flex direction="column">
@@ -70,22 +97,23 @@ export default function IndexPage({ creators }: IndexPageProps) {
             How does it work?
           </Heading>
           <Grid gridTemplateColumns="repeat(3, 1fr)" gap={10}>
-            <Flex direction="column" border="1px">
+            <Flex direction="column" border="1px" borderRadius="md">
               <Image fallbackSrc="https://i.imgur.com/2UFgYhq.png" h={52} />
               <Text p={5} fontSize="xl">
                 Sign in with your wallet
               </Text>
             </Flex>
-            <Flex direction="column" border="1px">
+            <Flex direction="column" border="1px" borderRadius="md">
               <Image fallbackSrc="https://i.imgur.com/EkHEXRH.png" h={52} />
               <Text p={5} fontSize="xl">
-              Edit your profile and make your first post
+                Edit your profile and make your first post
               </Text>
             </Flex>
-            <Flex direction="column" border="1px">
+            <Flex direction="column" border="1px" borderRadius="md">
               <Image fallbackSrc="https://i.imgur.com/8Ee8EMr.png" h={52} />
               <Text p={5} fontSize="xl">
-              Share your Yieldgate profile to start receiving donations from your supporters in yield!
+                Share your Yieldgate profile to start receiving donations from
+                your supporters in yield!
               </Text>
             </Flex>
           </Grid>
