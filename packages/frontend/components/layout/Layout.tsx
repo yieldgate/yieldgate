21d151355b5e07ddf2ab062ecef23030ffb2373d
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-24 13:57:47 +0200

    Add socials and email to footer

--- packages/frontend/components/layout/Layout.tsx
+++ packages/frontend/components/layout/Layout.tsx
@@ -1,7 +1,9 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, Grid, GridItem, Icon, Link } from '@chakra-ui/react'
 import React from 'react'
 import Head, { MetaProps } from './Head'
 import Navbar from './Navbar'
+import { BsTwitter, BsGithub } from 'react-icons/bs'
+import NextLink from 'next/link'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -16,9 +18,34 @@ const Layout = ({ children, customMeta }: LayoutProps): JSX.Element => {
         <Navbar />
       </header>
       <Box as="main">{children}</Box>
-      <Flex as="footer" borderTop="1px" p={5} justify="center" mt="12">
-        Built with ❤️ at ETHGlobal Amsterdam 🇳🇱
-      </Flex>
+      <Grid
+        as="footer"
+        borderTop="1px"
+        p={5}
+        mt="12"
+        templateColumns="300px 1fr 300px"
+      >
+        <Flex gap={5}>
+          <NextLink href="https://twitter.com/yieldgate" passHref>
+            <Link>
+              <Icon as={BsTwitter} boxSize="8" color="gray.700" />
+            </Link>
+          </NextLink>
+          <NextLink href="https://github.com/yieldgate" passHref>
+            <Link>
+              <Icon as={BsGithub} boxSize="8" color="gray.700" />
+            </Link>
+          </NextLink>
+        </Flex>
+        <GridItem placeSelf="center">
+          Built with ❤️ at ETHGlobal Amsterdam 🇳🇱
+        </GridItem>
+        <GridItem justifySelf="end">
+          <NextLink href="mailto:yieldgate@decentr.al" passHref>
+            <Link>yieldgate@decentr.al</Link>
+          </NextLink>
+        </GridItem>
+      </Grid>
     </>
   )
 }
