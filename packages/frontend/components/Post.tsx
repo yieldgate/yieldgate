d1ab57c67927694981b5c47f4867f3edf1457cbf
Raúl R. Pearson <raulrpearson@protonmail.com>
2022-04-24 05:40:46 +0200

    Improve locked content

--- packages/frontend/components/Post.tsx
+++ packages/frontend/components/Post.tsx
@@ -1,7 +1,16 @@
-import { Box, Divider, Heading } from '@chakra-ui/react'
+import {
+  Box,
+  Divider,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Icon,
+} from '@chakra-ui/react'
 import type { Post as PostType } from '@entities/Post.entity'
 import dayjs from 'dayjs'
 import md from 'markdown-it'
+import { FaLock } from 'react-icons/fa'
 
 function formatDateTime(date: string): string {
   return dayjs(date).format('MMM D, YYYY h:mm A')
@@ -9,21 +18,33 @@ function formatDateTime(date: string): string {
 
 function Lock(): JSX.Element {
   return (
-    <Box
+    <Grid
       h="200"
-      bg="gray.400"
-      sx={{ '> div + div': { mt: '8px' } }}
       p="5"
-      overflow="hidden"
-      mt={5}
       borderRadius="md"
+      bg="gray.200"
+      overflow="hidden"
+      templateColumns="1fr"
+      templateRows="1fr"
+      mt="5"
     >
-      <Box h="24px" w="95%" bg="gray.500" />
-      <Box h="24px" w="98%" bg="gray.500" />
-      <Box h="24px" w="93%" bg="gray.500" />
-      <Box h="24px" w="95%" bg="gray.500" />
-      <Box h="24px" w="76%" bg="gray.500" />
-    </Box>
+      <GridItem gridArea="1 / 1 / 1 / 1">
+        <Flex direction="column" gap={2}>
+          <Box h="24px" w="95%" bg="gray.300" />
+          <Box h="24px" w="98%" bg="gray.300" />
+          <Box h="24px" w="93%" bg="gray.300" />
+          <Box h="24px" w="95%" bg="gray.300" />
+          <Box h="24px" w="76%" bg="gray.300" />
+        </Flex>
+      </GridItem>
+      <GridItem
+        gridArea="1 / 1 / 1 / 1"
+        alignSelf="center"
+        justifySelf="center"
+      >
+        <Icon as={FaLock} color="gray.600" boxSize="24" />
+      </GridItem>
+    </Grid>
   )
 }
 
@@ -64,7 +85,7 @@ function Post({
       <Box as="span" color="gray.400" fontSize="lg">
         {formatDateTime(date)}
       </Box>
-      <Divider mt='4' />
+      <Divider mt="4" />
       <Box
         dangerouslySetInnerHTML={{ __html: md().render(content) }}
         mt={5}
