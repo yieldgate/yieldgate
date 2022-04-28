a393f6a380e4e5c69770661629e21dc95ff50896
Dennis Zoma <wottpal@users.noreply.github.com>
2022-04-28 15:53:07 +0200

    re-added rinkeby contract addresses

--- packages/frontend/addresses/index.ts
+++ packages/frontend/addresses/index.ts
@@ -1,6 +1,8 @@
 import { ContractAddresses_1337 } from './1337'
+import { ContractAddresses_4 } from './4'
 import { ContractAddresses_80001 } from './80001'
 export const ContractAddresses = {
+  '4': ContractAddresses_4,
   '1337': ContractAddresses_1337,
   '80001': ContractAddresses_80001,
 }
