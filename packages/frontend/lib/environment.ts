3f7573434d9b22a30d42e35f0ce6c39dc5fc807d
Dennis Zoma <dennis@yieldgate.xyz>
2022-06-22 19:41:40 +0200

    ci: Update vercel alias domain syntax to not use subdomains

--- packages/frontend/lib/environment.ts
+++ packages/frontend/lib/environment.ts
@@ -7,8 +7,6 @@ export const env = {
   defaultChain: process.env.NEXT_PUBLIC_DEFAULT_CHAIN!,
   supportedChains: JSON.parse(process.env.NEXT_PUBLIC_SUPPORTED_CHAINS!),
 
-  infuraApiKey: process.env.NEXT_PUBLIC_INFURA_API_KEY!,
-
   rpc: {
     hardhat: process.env.NEXT_PUBLIC_RPC_HARDHAT!,
     polygonMumbai: process.env.NEXT_PUBLIC_RPC_POLYGON_MUMBAI!,
