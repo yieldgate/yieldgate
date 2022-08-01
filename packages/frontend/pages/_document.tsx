223c933e59c51da255b69c52da0a25b170f25689
Raúl R. Pearson <raul@yieldgate.xyz>
2022-08-01 18:55:28 +0300

    Place icons under /favicons folder

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -3,11 +3,12 @@ import { Head, Html, Main, NextScript } from 'next/document'
 const Document = () => {
   return (
     <Html>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-      <link rel="manifest" href="/manifest.webmanifest" />
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicons/manifest.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
