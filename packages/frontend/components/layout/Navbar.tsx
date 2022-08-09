a3517c22542f94ef3883eeb95e8bf7e269a68856
Raúl R. Pearson <raul@yieldgate.xyz>
2022-08-09 13:41:00 +0300

    Readd profile link to Navbar

--- packages/frontend/components/layout/Navbar.tsx
+++ packages/frontend/components/layout/Navbar.tsx
@@ -1,8 +1,10 @@
 import { Box, Container, Flex, Image, Link } from '@chakra-ui/react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import NextLink from 'next/link'
+import { useAccount } from 'wagmi'
 
 function Navbar(): JSX.Element {
+  const { address } = useAccount()
   return (
     <Box borderBottom="1px">
       <Container maxW="5xl" py={4}>
@@ -12,7 +14,14 @@ function Navbar(): JSX.Element {
               <Image src="/images/logo-yieldgate.svg" boxSize={7} alt="Yieldgate Logo" />
             </Link>
           </NextLink>
-          <ConnectButton />
+          <div className="flex items-center gap-2">
+            <ConnectButton />
+            {address && (
+              <NextLink href={`/users/${(address || '').toLowerCase()}`}>
+                <a className="underline">My Profile</a>
+              </NextLink>
+            )}
+          </div>
         </Flex>
       </Container>
     </Box>
