0d16687bedc44a00f5e2600d26a13e9dd577f9de
Dennis Zoma <dennis@yieldgate.xyz>
2022-08-15 14:47:09 +0200

    hardhat: Fallback to `getSigners` for local deploy

--- packages/hardhat/deploy/001_deploy_yieldgate.ts
+++ packages/hardhat/deploy/001_deploy_yieldgate.ts
@@ -1,16 +1,23 @@
 import { DeployFunction } from 'hardhat-deploy/types'
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { AaveAddresses } from 'shared/aaveAddresses'
+import { AaveAddresses } from '../shared/aaveAddresses'
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre
+  const { deployments, getNamedAccounts, ethers } = hre
   const { deploy } = deployments
+  const chainId = await hre.getChainId()
 
-  const { deployer } = await getNamedAccounts()
+  let { deployer } = await getNamedAccounts()
+  if (!deployer && chainId === '1337') {
+    const [signer] = await ethers.getSigners()
+    deployer = signer.address
+  } else if (!deployer) {
+    throw new Error(`No deployer address defined for network ${hre.network.name}`)
+  }
   console.log(`Deploying as ${deployer}…`)
 
   const aave = AaveAddresses[hre.network.name]
-  if (aave === undefined) {
+  if (!aave) {
     throw new Error(`No Aave addresses for network ${hre.network.name}`)
   }
 
