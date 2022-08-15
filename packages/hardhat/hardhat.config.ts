1fc477847297a236d020ddf5bbf095ec6847e065
Dennis Zoma <dennis@yieldgate.xyz>
2022-08-15 14:46:43 +0200

    hardhat: Rename environment variables for clarity and future alignment with frontend

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -5,31 +5,41 @@ import { HardhatUserConfig } from 'hardhat/types'
 import path from 'path'
 dotenv.config()
 
+const accounts = [
+  ...(process.env.PRIVATE_KEY_DEPLOYER ? [`${process.env.PRIVATE_KEY_DEPLOYER}`] : []),
+]
+
 const config: HardhatUserConfig = {
   solidity: '0.8.10',
   networks: {
     hardhat: {
       chainId: 1337,
-      // chainId: 80001,
       allowUnlimitedContractSize: false,
       blockGasLimit: 20000000, // 20 million
+      // chainId: 80001,
       // forking: {
-      //   url: "https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_MUMBAI}",
-      //   url: "https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_RINKEBY}",
-      //   url: "https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_MAINNET}",
+      //   url: process.env.RPC_80001 || 'https://rpc.ankr.com/polygon_mumbai',
       // },
     },
     rinkeby: {
       chainId: 4,
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_RINKEBY}`,
-      accounts: [
-        ...(process.env.PRIVATE_KEY_RINKEBY ? [`${process.env.PRIVATE_KEY_RINKEBY}`] : []),
-      ],
+      url: process.env.RPC_4 || 'https://rpc.ankr.com/eth_rinkeby',
+      accounts,
+    },
+    goerli: {
+      chainId: 5,
+      url: process.env.RPC_5 || 'https://rpc.ankr.com/eth_goerli',
+      accounts,
     },
     mumbai: {
       chainId: 80001,
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_MUMBAI}`,
-      accounts: [...(process.env.PRIVATE_KEY_MUMBAI ? [`${process.env.PRIVATE_KEY_MUMBAI}`] : [])],
+      url: process.env.RPC_80001 || 'https://rpc.ankr.com/polygon_mumbai',
+      accounts,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
   },
   typechain: {
