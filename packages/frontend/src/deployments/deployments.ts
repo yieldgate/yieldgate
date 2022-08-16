bc3e05c7fba75e567b7a73e198c6c515fb8a7d14
Dennis Zoma <dennis@yieldgate.xyz>
2022-08-16 16:47:23 +0200

    frontend: revert the previous improvement as removing the synchrony leaded to some issues

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -5,22 +5,12 @@ import { HardhatExport } from 'src/types/hardhat'
  * Dynamically aggregating all deployments (addresses, abis)
  */
 
-const getDeploymentForChainId = async (chainId: number) => {
-  try {
-    return await import(`./${chainId}.json`)
-  } catch (e) {
-    throw new Error(
-      `Can't find deployment for supported chain (${chainId}) under 'src/deployments/${chainId}.json'.`
-    )
-  }
-}
-
 export type DeploymentsType = { [_: number]: Promise<HardhatExport> }
 
 export const deployments: DeploymentsType = env.supportedChains.reduce(
   (acc: DeploymentsType, chainId: number) => ({
     ...acc,
-    [chainId]: getDeploymentForChainId(chainId),
+    [chainId]: import(`./${chainId}.json`),
   }),
   {}
 )
