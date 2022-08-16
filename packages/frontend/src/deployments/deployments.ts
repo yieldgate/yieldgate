59f92704f59e887ee9e51f3e21af4165cd1042e3
Dennis Zoma <dennis@yieldgate.xyz>
2022-08-16 16:34:48 +0200

    frontend: More descriptive error message if deployment of supported chain can’t be found

--- packages/frontend/src/deployments/deployments.ts
+++ packages/frontend/src/deployments/deployments.ts
@@ -4,11 +4,23 @@ import { HardhatExport } from 'src/types/hardhat'
 /**
  * Dynamically aggregating all deployments (addresses, abis)
  */
+
+const getDeploymentForChainId = async (chainId: number) => {
+  try {
+    return await import(`./${chainId}.json`)
+  } catch (e) {
+    throw new Error(
+      `Can't find deployment for supported chain (${chainId}) under 'src/deployments/${chainId}.json'.`
+    )
+  }
+}
+
 export type DeploymentsType = { [_: number]: Promise<HardhatExport> }
+
 export const deployments: DeploymentsType = env.supportedChains.reduce(
   (acc: DeploymentsType, chainId: number) => ({
     ...acc,
-    [chainId]: import(`./${chainId}.json`),
+    [chainId]: getDeploymentForChainId(chainId),
   }),
   {}
 )
