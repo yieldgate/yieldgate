157bf89c3293538a6b234b0b44a931d7d3181b20
Dennis Zoma <dennis@yieldgate.xyz>
2022-08-16 16:48:53 +0200

    frontend: Rewrite `wagmiClient` to be completely dynamic with `supportedChains` environment variable

--- packages/frontend/src/lib/wagmiClient.ts
+++ packages/frontend/src/lib/wagmiClient.ts
@@ -1,44 +1,39 @@
 import { getDefaultWallets } from '@rainbow-me/rainbowkit'
-import { allChains, Chain, chain, configureChains, createClient } from 'wagmi'
+import { allChains, chain, Chain, configureChains, createClient } from 'wagmi'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { publicProvider } from 'wagmi/providers/public'
-
 import { env } from './environment'
 
 /**
  * Wagmi.sh Configuration (https://wagmi.sh/docs)
  */
 
-export const rpcsByChainId: {
-  [chainId: number]: string
-} = {
-  [1337]: env.rpc.hardhat,
-  [chain.polygonMumbai.id]: env.rpc.polygonMumbai,
-  [chain.rinkeby.id]: env.rpc.rinkeby,
-  [chain.mainnet.id]: env.rpc.mainnet,
-  [chain.polygon.id]: env.rpc.polygon,
-}
+export const defaultChain: Chain | undefined = allChains.find(
+  (chain) => env.defaultChain === chain.id
+)
 
-export const defaultChain: Chain = allChains.filter((chain) => env.defaultChain === chain.id)[0]
 export const supportedChains: Chain[] = allChains.filter((chain) =>
   env.supportedChains.includes(chain.id)
 )
 
 export const { chains, provider } = configureChains(
-  Array.from(new Set([defaultChain, chain.mainnet, ...supportedChains])),
+  Array.from(new Set([defaultChain, chain.mainnet, ...supportedChains])).filter(Boolean) as Chain[],
   [
     jsonRpcProvider({
       rpc: (chain) => {
-        const rpcUrl = rpcsByChainId?.[chain.id]
-        return rpcUrl ? { http: rpcUrl } : null
+        const rpcUrl = env.rpcUrls[chain.id as keyof typeof env.rpcUrls]
+        if (!rpcUrl) {
+          throw new Error(`No RPC provided for chain ${chain.id}`)
+        }
+        return { http: rpcUrl }
       },
     }),
     publicProvider(),
   ]
 )
 
-export const { connectors } = getDefaultWallets({
-  appName: 'Yieldgate',
+const { connectors } = getDefaultWallets({
+  appName: 'YieldGate',
   chains,
 })
 
