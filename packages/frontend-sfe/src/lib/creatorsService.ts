d6489a7d882b0a61ec20373209fee2e93b4ddea8
Dennis Zoma <dennis@yieldgate.xyz>
2022-09-11 15:26:25 +0200

    frontend-sfe: Remove mongodb occurences

--- packages/frontend-sfe/src/lib/creatorsService.ts
+++
@@ -1,9 +0,0 @@
-import { Db } from 'mongodb'
-
-export const createCreator = async (db: Db, address: string, data?: any) => {
-  return await db.collection('creators').insertOne({
-    address,
-    supporters: [],
-    ...data,
-  })
-}
