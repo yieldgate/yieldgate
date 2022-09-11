ed774c17758b293ee965f9f1ba404c68c1dcaa4d
Dennis Zoma <dennis@yieldgate.xyz>
2022-09-11 15:29:16 +0200

    frontend-sfe: Add twin.macro (tailwindcss + styled components)

--- packages/frontend-sfe/src/pages/_document.tsx
+++ packages/frontend-sfe/src/pages/_document.tsx
@@ -1,20 +1,40 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import { extractCritical } from '@emotion/server'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Fragment, ReactFragment } from 'react'
 
-const Document = () => {
-  return (
-    <Html>
-      <Head>
-        <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
-        <link rel="manifest" href="/favicons/manifest.webmanifest" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    const critical = extractCritical(initialProps.html)
+    initialProps.html = critical.html
+    initialProps.styles = (
+      <Fragment>
+        {initialProps.styles}
+        <style
+          data-emotion-css={critical.ids.join(' ')}
+          dangerouslySetInnerHTML={{ __html: critical.css }}
+        />
+      </Fragment>
+    ) as any as ReactFragment
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Favicon */}
+          <link rel="icon" href="/favicons/favicon.ico" sizes="any" />
+          <link rel="icon" href="/favicons/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+          <link rel="manifest" href="/favicons/manifest.webmanifest" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
-
-export default Document
--- packages/frontend-sfe/src/pages/index.tsx
+++ packages/frontend-sfe/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import 'twin.macro'
 
 export interface IndexPageProps {}
 export default function IndexPage() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center space-y-4 h-screen">
+      <div tw="flex flex-col justify-center items-center space-y-4 h-screen">
         <div>Stake for Earth</div>
         <ConnectButton />
       </div>
