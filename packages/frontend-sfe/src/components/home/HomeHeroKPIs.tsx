0d55ee1ee2da9499084cc443e1637dccc7d48e0c
Dennis Zoma <dennis@yieldgate.xyz>
2022-10-05 14:51:14 +0200

    frontend-sfe: Add loading animation (and demo-loading) for KPI-elements

--- packages/frontend-sfe/src/components/home/HomeHeroKPIs.tsx
+++ packages/frontend-sfe/src/components/home/HomeHeroKPIs.tsx
@@ -1,29 +1,40 @@
 import { Wrapper } from '@components/layout/Wrapper'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { NumericFormat, NumericFormatProps } from 'react-number-format'
+import { SpinnerDiamond } from 'spinners-react'
 import 'twin.macro'
+import tw, { theme } from 'twin.macro'
 
 export interface HomeHeroKPIsProps {}
 export const HomeHeroKPIs: FC<HomeHeroKPIsProps> = () => {
+  const [isDemoLoading, setIsDemoLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsDemoLoading(false)
+    }, 1500)
+  }, [])
+
   return (
     <>
       <h2 tw="sr-only">Stake for Earth KPIs</h2>
       <div tw="bg-gray-100">
         <Wrapper noVerticalPadding>
-          <div tw="grid divide-y divide-gray-400 -mx-4 sm:(-mx-8) lg:(grid-cols-3 divide-y-0 divide-x mx-0)">
+          <div tw="grid divide-y divide-gray-300 -mx-4 sm:(-mx-8) lg:(grid-cols-3 divide-y-0 divide-x mx-0)">
             <HomeHeroKPIItem
               label="Total amount"
               value={11140456}
               numericFormatProps={{ prefix: '$' }}
               tw="lg:pl-0"
+              isLoading={isDemoLoading}
             />
-            <HomeHeroKPIItem label="Number of stakers" value={23140} />
+            <HomeHeroKPIItem label="Number of stakers" value={23140} isLoading={isDemoLoading} />
             <HomeHeroKPIItem
               label="Last stake"
               description="10 min ago by 0x23rw...i29d"
               value={30}
               numericFormatProps={{ prefix: '$', fixedDecimalScale: true, decimalScale: 2 }}
               tw="lg:pr-0"
+              isLoading={isDemoLoading}
             />
           </div>
         </Wrapper>
@@ -37,30 +48,44 @@ export interface HomeHeroKPIItemProps {
   label: string
   description?: string
   numericFormatProps?: NumericFormatProps
+  isLoading?: boolean
 }
 export const HomeHeroKPIItem: FC<HomeHeroKPIItemProps> = ({
   value,
   label,
   description,
   numericFormatProps,
+  isLoading,
   ...props
 }) => {
   return (
     <>
       <div
-        tw="flex flex-col space-y-1 xs:(flex-row space-x-2 space-y-0) items-center justify-between px-14 py-6"
+        tw="flex flex-col min-h-[6.25rem] space-y-1 xs:(flex-row space-x-2 space-y-0) items-center justify-between px-14 py-6"
         {...props}
       >
-        <div tw="text-sm">{label}</div>
+        <div css={[tw`text-sm`, isLoading ? tw`text-gray-800 animate-pulse` : tw`text-black`]}>
+          {label}
+        </div>
         <div tw="flex flex-col whitespace-nowrap text-center xs:(text-right)">
-          <NumericFormat
-            value={value}
-            displayType={'text'}
-            thousandSeparator={true}
-            tw="text-3xl font-display font-bold"
-            {...numericFormatProps}
-          />
-          {description && <div tw="text-xs opacity-75">{description}</div>}
+          {isLoading ? (
+            <SpinnerDiamond
+              size={34}
+              color={theme('colors.gray.500')}
+              secondaryColor={theme('colors.gray.300')}
+            />
+          ) : (
+            <>
+              <NumericFormat
+                value={value}
+                displayType={'text'}
+                thousandSeparator={true}
+                tw="text-3xl font-display font-bold"
+                {...numericFormatProps}
+              />
+              {description && <div tw="text-xs opacity-75">{description}</div>}
+            </>
+          )}
         </div>
       </div>
     </>
