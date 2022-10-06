af4343660be55727976f68fdb5eab32ce9dec20b
Dennis Zoma <dennis@yieldgate.xyz>
2022-10-06 23:00:25 +0200

    frontend-sfe: Improve decorative sidebar on stake-subpage

--- packages/frontend-sfe/src/pages/stake.tsx
+++ packages/frontend-sfe/src/pages/stake.tsx
@@ -1,10 +1,12 @@
 import { StakingLayout } from '@components/staking/StakingLayout'
+import { NextSeo } from 'next-seo'
 import 'twin.macro'
 
 export interface StakingPageProps {}
 export default function StakingPage() {
   return (
     <>
+      <NextSeo title="Stake & Dontation Form" />
       <StakingLayout>
         {/* TODO */}
         StakingPage
