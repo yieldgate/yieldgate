bd9641dca33e54ffc8188417b45ecedd7d5ae5df
Dennis Zoma <dennis@yieldgate.xyz>
2022-10-11 16:07:11 +0200

    frontend-sfe: Add bundle analyzer

--- packages/frontend-sfe/next.config.js
+++ packages/frontend-sfe/next.config.js
@@ -4,9 +4,16 @@
 /**
  * @type {import('next').NextConfig}
  **/
-const nextConfig = {}
+let nextConfig = {}
 
 // eslint-disable-next-line
 const withTM = require('next-transpile-modules')(['@yieldgate/contracts'])
+nextConfig = withTM(nextConfig)
 
-module.exports = withTM(nextConfig)
+// eslint-disable-next-line
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+nextConfig = withBundleAnalyzer(nextConfig)
+
+module.exports = nextConfig
