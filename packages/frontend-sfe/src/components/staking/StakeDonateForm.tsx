87e6c721325398ce8083daa6a5390f41e4d1c2fc
Dennis Zoma <dennis@yieldgate.xyz>
2022-10-18 11:14:13 +0200

    frontend-sfe: Improve & refactor staking-form

--- packages/frontend-sfe/src/components/staking/StakeDonateForm.tsx
+++ packages/frontend-sfe/src/components/staking/StakeDonateForm.tsx
@@ -4,7 +4,7 @@ import { formatUnits } from 'ethers/lib/utils'
 import Image from 'next/image'
 import usdcSvg from 'public/icons/tokens/usdc.svg'
 import { FC, SyntheticEvent, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, UseFormReturn } from 'react-hook-form'
 import { NumericFormat } from 'react-number-format'
 import 'twin.macro'
 import { useAccount, useBalance, useNetwork } from 'wagmi'
@@ -16,30 +16,15 @@ import {
 } from './StakingStepperItemSharedComponents'
 import { StakingViewStakeDonateProps } from './StakingViewStakeDonate'
 
+type StakeDonateFormValues = {
+  stakingAmount: string
+}
+
 export interface StakeDonateFormProps extends StakingViewStakeDonateProps {}
 export const StakeDonateForm: FC<StakeDonateFormProps> = ({ mode, onGoNext }) => {
   const [isLoading, setIsLoading] = useState(false)
-  const form = useForm({ mode: 'onChange' })
+  const form = useForm<StakeDonateFormValues>({ mode: 'onChange' })
   const { isValid } = form.formState
-
-  // Fetch USDC balance
-  const { address } = useAccount()
-  const { chain } = useNetwork()
-  const {
-    data: balance,
-    isError,
-    isLoading: balanceIsLoading,
-  } = useBalance({
-    addressOrName: address,
-    watch: true,
-    // TODO
-    token:
-      chain?.id === 137
-        ? '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
-        : chain?.id === 80001
-        ? '0x9aa7fEc87CA69695Dd1f879567CcF49F3ba417E2'
-        : constants.AddressZero,
-  })
 
   // Handle form submit (staking action)
   const onSubmit = async (e: SyntheticEvent) => {
@@ -57,63 +42,12 @@ export const StakeDonateForm: FC<StakeDonateFormProps> = ({ mode, onGoNext }) =>
           {mode === 'donate' ? 'Donation Amount' : 'Staking Amount'}
         </StakingStepperItemContentBoxHeadline>
         <StakingStepperItemContentBoxSubtitle>
-          Explanation Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
-          doloremque laudantium, totam rem.
+          Set the amount you want to put into the climate pool to participate in carbon credits
+          burning. You can always unstake.
         </StakingStepperItemContentBoxSubtitle>
 
         <form onSubmit={onSubmit}>
-          <label htmlFor="stakingAmount" tw="sr-only">
-            Staking Amount
-          </label>
-          <div tw="relative">
-            {/* Stake Amount Input */}
-            <input
-              type="text"
-              id="stakingAmount"
-              placeholder="100.00"
-              tw="w-full bg-white text-2xl font-medium text-black border border-gray-300 rounded leading-none py-5 pl-4 pr-[5rem] outline-none focus:(ring-offset-2 ring-2 ring-sky-500)"
-              {...form.register('stakingAmount', {
-                required: true,
-                min: 1,
-                pattern: {
-                  value: /^(0|[1-9]\d*)(\.\d+)?$/,
-                  message: 'Please enter a valid number',
-                },
-              })}
-            />
-
-            {/* Unit & Balance Input Suffix */}
-            <div tw="flex flex-col items-end space-y-1.5 absolute right-4 top-1/2 -translate-y-1/2 z-10">
-              <div tw="flex items-center space-x-1 text-lg font-medium text-black tracking-tight leading-none">
-                <Image src={usdcSvg} width={20} height={20} alt="USDC Token Logo" />
-                <div>USDC</div>
-              </div>
-              <div tw="flex justify-end items-baseline text-sm leading-none h-4 space-x-1.5">
-                {balance !== undefined && (
-                  <>
-                    <div tw="text-gray-600">
-                      Balance:{' '}
-                      <NumericFormat
-                        value={formatUnits(balance.value, 6)}
-                        displayType={'text'}
-                        decimalScale={2}
-                        thousandSeparator={true}
-                      />
-                    </div>
-                    <button
-                      type="button"
-                      tw="text-green-500 font-semibold border border-green-200 hover:(border-green-400) rounded p-0.5"
-                      onClick={() => {
-                        form.setValue('stakingAmount', balance.formatted)
-                      }}
-                    >
-                      Max
-                    </button>
-                  </>
-                )}
-              </div>
-            </div>
-          </div>
+          <StakeDonateAmountInputField form={form} />
 
           <StakingStepperItemContentBoxDivider />
 
@@ -130,6 +64,89 @@ export const StakeDonateForm: FC<StakeDonateFormProps> = ({ mode, onGoNext }) =>
           </BaseButtonGroup>
         </form>
       </StakingStepperItemContentBox>
+    </>
+  )
+}
+
+export interface StakeDonateAmountInputFieldProps {
+  form: UseFormReturn<StakeDonateFormValues, any>
+}
+export const StakeDonateAmountInputField: FC<StakeDonateAmountInputFieldProps> = ({ form }) => {
+  const { errors } = form.formState
+  const { address } = useAccount()
+  const { chain } = useNetwork()
+  const { data: balance } = useBalance({
+    addressOrName: address,
+    watch: true,
+    // TODO
+    token:
+      chain?.id === 137
+        ? '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
+        : chain?.id === 80001
+        ? '0x9aa7fEc87CA69695Dd1f879567CcF49F3ba417E2'
+        : constants.AddressZero,
+  })
+
+  return (
+    <>
+      <div tw="relative">
+        {/* Input Field */}
+        <label htmlFor="stakingAmount" tw="sr-only">
+          Staking Amount
+        </label>
+        <input
+          type="text"
+          id="stakingAmount"
+          placeholder="100.00"
+          tw="w-full bg-white text-2xl font-medium text-black border border-gray-300 rounded leading-none py-5 pl-4 pr-[5rem] outline-none focus:(ring-offset-2 ring-2 ring-sky-500)"
+          {...form.register('stakingAmount', {
+            required: true,
+            min: {
+              value: 1,
+              message: 'Please enter an amount ≥ 1',
+            },
+            pattern: {
+              value: /^(0|[1-9]\d*)(\.\d+)?$/,
+              message: 'Please enter a valid number',
+            },
+          })}
+        />
+
+        <div tw="flex flex-col items-end space-y-2 absolute right-4 top-1/2 -translate-y-1/2 z-10">
+          {/* Unit (USDC) */}
+          <div tw="flex items-center space-x-1 text-lg font-medium text-black tracking-tight leading-none">
+            <Image src={usdcSvg} width={18} height={18} alt="USDC Token Logo" />
+            <div>USDC</div>
+          </div>
+
+          {/* Balance and "Max"-Button */}
+          {balance !== undefined && (
+            <div tw="flex justify-end items-baseline text-sm leading-none h-4 space-x-1.5">
+              <div tw="text-gray-600">
+                Balance:{' '}
+                <NumericFormat
+                  value={formatUnits(balance.value, 6)}
+                  displayType={'text'}
+                  decimalScale={2}
+                  thousandSeparator={true}
+                />
+              </div>
+              <button
+                type="button"
+                tw="text-green-500 font-semibold border border-green-200 hover:(border-green-300) rounded-md py-0.5 px-1"
+                onClick={() => {
+                  form.setValue('stakingAmount', balance.formatted)
+                }}
+              >
+                Max
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* Error Message */}
+      <div tw="mt-2 text-xs font-semibold text-amber-500">{errors.stakingAmount?.message}</div>
     </>
   )
 }
