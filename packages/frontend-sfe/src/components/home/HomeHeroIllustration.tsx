32a653016dda55222a50ee23a65c94f63d00c749
Dennis Zoma <dennis@yieldgate.xyz>
2022-10-18 12:13:54 +0200

    frontend-sfe: Demo most recent hero illustration draft

--- packages/frontend-sfe/src/components/home/HomeHeroIllustration.tsx
+++ packages/frontend-sfe/src/components/home/HomeHeroIllustration.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import illustrationImg from 'public/images/hero-illustration-draft.png'
+import illustrationImg from 'public/images/hero-illustration-draft-7.png'
 import { FC } from 'react'
 import 'twin.macro'
 
@@ -8,14 +8,12 @@ export const HomeHeroIllustration: FC<HomeHeroIllustrationProps> = ({ ...props }
   return (
     <>
       <div
-        tw="max-h-[90%] w-full flex flex-col absolute bottom-0 right-0 z-0 select-none overflow-hidden"
+        tw="max-h-[100%] w-full flex flex-col absolute bottom-0 right-0 z-0 select-none overflow-hidden"
         {...props}
       >
         <Image
           src={illustrationImg}
           priority={true}
-          height={1023}
-          width={1888}
           objectFit="contain"
           objectPosition="right bottom"
           alt="Decorative Earth Illustration by Liam Cobb"
